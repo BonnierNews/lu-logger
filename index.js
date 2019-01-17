f576b63205d240ebf6f966ebc561f9c1b5c59efc
Jens Carlén <jens.carlen@bonniernews.se>
2019-01-17 08:41:18 +0100

    Fixes

--- index.js
+++ index.js
@@ -12,51 +12,57 @@ require("winston-syslog").Syslog; // eslint-disable-line no-unused-expressions
 
 const PromTransport = require("./lib/prom-transport");
 const config = appConfig.logging;
-const transports = [new PromTransport()];
-
-const defaultFormatter = format.printf((info) => `${info.timestamp} - ${info.level}: ${info.message}`);
-const formatter = config.logJson ? format.logstash() : defaultFormatter;
-
-if (config.log === "file") {
-  const fileName = path.join(process.cwd(), "logs", `${appConfig.envName}.log`);
-  const options = config.truncateLog ? {flags: "w"} : {flags: "a"};
-  transports.push(new winston.transports.File({
-    filename: fileName,
-    options: options
-  }));
-}
-
-if (config.log === "stdout") {
-  transports.push(new winston.transports.Console());
-}
-
-if (config.sysLogOpts) {
-  transports.push(new winston.transports.Syslog(Object.assign({
-    localhost: process.env.HOSTNAME,
-    app_name: callingAppName, // eslint-disable-line camelcase
-    eol: "\n"
-  }, config.sysLogOpts)));
-}
 
 function logLevel(info) {
   info.logLevel = info.level;
   return info;
 }
 
-const logger = winston.createLogger({
-  level: config.logLevel || "info",
-  levels: logLevels,
-  transports: transports,
-  format: format.combine(
-    format.metadata({key: "metaData"}),
-    format.timestamp(),
-    format(logLevel)(),
-    format(splatEntry)(),
-    formatter
-  ),
-  defaultMeta: {appName: callingAppName},
-});
+function createLogger() {
+  const transports = [new PromTransport()];
+
+  const defaultFormatter = format.printf((info) => `${info.timestamp} - ${info.level}: ${info.message}`);
+  const formatter = config.logJson ? format.logstash() : defaultFormatter;
+
+  if (config.log === "file") {
+    const fileName = path.join(process.cwd(), "logs", `${appConfig.envName}.log`);
+    const options = config.truncateLog ? {flags: "w"} : {flags: "a"};
+    transports.push(new winston.transports.File({
+      filename: fileName,
+      options: options
+    }));
+  }
+
+  if (config.log === "stdout") {
+    transports.push(new winston.transports.Console());
+  }
+
+  if (config.sysLogOpts) {
+    transports.push(new winston.transports.Syslog(Object.assign({
+      localhost: process.env.HOSTNAME,
+      app_name: callingAppName, // eslint-disable-line camelcase
+      eol: "\n"
+    }, config.sysLogOpts)));
+  }
+
+
+  const logger = winston.createLogger({
+    level: config.logLevel || "info",
+    levels: logLevels,
+    transports: transports,
+    format: format.combine(
+      format.metadata({key: "metaData"}),
+      format.timestamp(),
+      format(logLevel)(),
+      format(splatEntry)(),
+      formatter
+    )
+  });
+
+  return logger;
+}
 
 module.exports = {
-  logger
+  logger: createLogger(),
+  createLogger
 };
--- test/logger-test.js
+++ test/logger-test.js
@@ -10,35 +10,35 @@ describe("logger", () => {
     logger.info("foobar");
     const log = transport.logs.shift();
     log.should.include({level: "info", message: "foobar"});
-    log.meta.should.eql({});
+    log.metaData.should.eql({});
   });
 
   it("should log with meta", () => {
     logger.info("some message", {"some": "info"});
     const log = transport.logs.shift();
     log.should.include({level: "info", message: "some message"});
-    log.meta.should.eql({"some": "info"});
+    log.metaData.should.eql({"some": "info"});
   });
 
   it("should log splat multiple arguments with meta", () => {
     logger.info("one", "two", "three", "four", {"some": "info"});
     const log = transport.logs.shift();
     log.should.include({level: "info", message: "one two three four"});
-    log.meta.should.eql({"some": "info"});
+    log.metaData.should.eql({"some": "info"});
   });
 
   it("should log splat multiple arguments without meta", () => {
     logger.info("one", "two", "three", "four");
     const log = transport.logs.shift();
     log.should.include({level: "info", message: "one two three four"});
-    log.meta.should.eql({});
+    log.metaData.should.eql({});
   });
 
   it("should log splat multiple objects with meta", () => {
     logger.info("one", "two", {three: 3, four: 4}, {"correlationId": "coobar"});
     const log = transport.logs.shift();
     log.should.include({level: "info", message: "one two { three: 3, four: 4 }"});
-    log.meta.should.eql({correlationId: "coobar"});
+    log.metaData.should.eql({correlationId: "coobar"});
   });
 
   describe("levels", () => {
